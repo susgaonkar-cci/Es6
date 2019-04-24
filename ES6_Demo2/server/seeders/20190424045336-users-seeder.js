1212eedfcaf5b690eef99d8dba9b85e7dcb35af2
susgaonkar-cci <sanatusg@gmail.com>
2019-04-24 10:35:58 +0530

    Create 20190424045336-users-seeder.js

---
+++ ES6_Demo2/server/seeders/20190424045336-users-seeder.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+up: (queryInterface, Sequelize) => {
+
+
+      // Example:
+      // return queryInterface.bulkInsert('users', [{
+      //   username: 'sanat',
+      //   password:'12333',
+      //   createdAt:new Date(),
+      //   updatedAt:new Date()
+      // }], {});
+  
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
